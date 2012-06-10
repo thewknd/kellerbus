898104d1a8727227a8ffa832bf166d2f54da78a6
thewknd <info@13mm.ch>
2012-06-10 15:12:10 +0200

    .

--- kellerbus.h
+++ kellerbus.h
@@ -8,35 +8,37 @@ This work is licensed under the Creative Commons Attribution-ShareAlike 3.0 Unpo
 #ifndef _H_KELLERBUS
 #define _H_KELLERBUS
 
-#define RS_OK                   		0
-#define RS_ERROR 										-1
-#define RS_TIMEOUT          				-4
-#define RS_BADDATA 									-5
+
+// Error codes
+#define RS_OK												0
+#define RS_ERROR										-1
+#define RS_TIMEOUT									-4
+#define RS_BADDATA									-5
 #define SW_INVALIDPARAM							-6
 #define RS_TXERROR									-7
-#define RS_BADCRC 									-8
+#define RS_BADCRC										-8
 #define DEVICE_NONFUNCTION					-10
 #define DEVICE_INCPARAMETERS				-11
 #define DEVICE_ERRONEOUSDATA				-12
 #define DEVICE_INIT									-13
 
-#define COMM_TX_MAX                 20
-#define COMM_RX_MAX                 260
+#define COMM_TX_MAX									20
+#define COMM_RX_MAX									260
 
-#define P_BAR												0 		// Bar
-#define P_MBAR											1 		// mBar
-#define P_PSI												2 		// Psi
-#define P_PA												3 		// Pascal
-#define P_HPA												1 		// hPascal
-#define P_KPA												4 		// kPascal
-#define P_MPA												5 		// MPascal
-#define P_MH2O											6 		// mH2O
-#define P_MWS												6 		// mWs Meter Wassersäule
-#define P_MWG												6 		// mWg meters, water gauge
-#define P_INHG											7 		// inHg
-#define P_MMHG											8 		// mmHg
-#define P_INH2O											9 		// inH2O
-#define P_TORR											8 		// Torr
+#define P_BAR												0			// Bar
+#define P_MBAR											1			// mBar
+#define P_PSI												2			// Psi
+#define P_PA												3			// Pascal
+#define P_HPA												1			// hPascal
+#define P_KPA												4			// kPascal
+#define P_MPA												5			// MPascal
+#define P_MH2O											6			// mH2O
+#define P_MWS												6			// mWs Meter Wassersäule
+#define P_MWG												6			// mWg meters, water gauge
+#define P_INHG											7			// inHg
+#define P_MMHG											8			// mmHg
+#define P_INH2O											9			// inH2O
+#define P_TORR											8			// Torr
 #define P_FTH2O											10		// ftH2O
 
 #define T_DEGC											0			// °C Celsius
@@ -44,7 +46,7 @@ This work is licensed under the Creative Commons Attribution-ShareAlike 3.0 Unpo
 #define T_DEGF											2			// °F Fahreinheit
 #define T_DEGR											3			// °R Rankine
 
-#define MAX_CHANNELS                6
+#define MAX_CHANNELS								6
 
 #define CH_0												0
 #define CH_P1												1
@@ -53,51 +55,51 @@ This work is licensed under the Creative Commons Attribution-ShareAlike 3.0 Unpo
 #define CH_TOB1											4
 #define CH_TOB2											5
 
-#define KB_DEBUG										1
+#define KB_DEBUG										0
 
 class CKellerBus
 {
-  private:
-    HardwareSerial *Comm;
-  
-    CRC checksum;
-  
-    uint16_t baudrate;
-    uint8_t RTS_PIN;
-    uint16_t timeout;
-  
-    uint8_t TxBuffer[COMM_TX_MAX];
-    uint8_t RxBuffer[COMM_TX_MAX + COMM_RX_MAX];
-  
-    int8_t Error;
-  
-    uint8_t device;
-  
-    void Open();
-    void Close();
-  
-    void TransferData(uint8_t, uint8_t);
-    float readChannel(uint8_t);
-    float pressureConversion(float, uint8_t);
-    float temperatureConversion(float, uint8_t);
-  
-  public:  
-    CKellerBus(HardwareSerial*, uint16_t, uint8_t, uint16_t);
-  
-    int8_t getError();
-  
-    void initDevice(uint8_t _device); 
-    void initDevice(uint8_t, uint8_t*, uint8_t*, uint8_t*, uint8_t*, uint8_t*, uint8_t*); 
-  
-    uint8_t getDevice();
-    uint32_t getSerialnumber();
-  
-    float getCH0();
-    float getP1(uint8_t);
-    float getP2(uint8_t);
-    float getTOB1(uint8_t);
-    float getTOB2(uint8_t);
-    float getT(uint8_t);    
+	private:
+		HardwareSerial *Comm;
+	
+		CRC checksum;
+	
+		uint16_t baudrate;
+		uint8_t RTS_PIN;
+		uint16_t timeout;
+	
+		uint8_t TxBuffer[COMM_TX_MAX];
+		uint8_t RxBuffer[COMM_TX_MAX + COMM_RX_MAX];
+	
+		int8_t Error;
+	
+		uint8_t device;
+	
+		void Open();
+		void Close();
+	
+		void TransferData(uint8_t, uint8_t);
+		float readChannel(uint8_t);
+		float pressureConversion(float, uint8_t);
+		float temperatureConversion(float, uint8_t);
+	
+	public:	 
+		CKellerBus(HardwareSerial*, uint16_t, uint8_t, uint16_t);
+	
+		int8_t getError();
+	
+		void initDevice(uint8_t _device); 
+		void initDevice(uint8_t, uint8_t*, uint8_t*, uint8_t*, uint8_t*, uint8_t*, uint8_t*); 
+	
+		uint8_t getDevice();
+		uint32_t getSerialnumber();
+	
+		float getCH0();
+		float getP1(uint8_t);
+		float getP2(uint8_t);
+		float getTOB1(uint8_t);
+		float getTOB2(uint8_t);
+		float getT(uint8_t);
 };
 #endif
 
